81f2dc5bb565800f67fb1fdc6ae59cac400705f1
lodry <jan_ph@op.pl>
2020-03-31 01:08:29 +0300

    Insert sort

---
+++ SiAD/InsertSecond.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiAD
+{
+    class InsertSecond : ISort
+    {
+        public InsertSecond()
+        {
+          //  this.SComparator = this.less;
+        }
+        public delegate bool op(int a, int b);
+        public op SComparator;
+        public int[] sort(int[] IntArray)
+        {
+            for (int cur = 1; cur < IntArray.Length; cur++)
+            {
+                int currentElement = IntArray[cur], previos;
+                for (previos = (cur - 1); previos >= 0 && SComparator(IntArray[previos], currentElement); previos--)
+                {
+                    IntArray[previos + 1] = IntArray[previos];
+                }
+                IntArray[previos + 1] = currentElement;
+            }
+            return IntArray;
+        }
+        public bool great(int a, int b) => a > b;
+        public bool less(int a, int b) => a < b;
+    }
+}
--- SiAD/Program.cs
+++ SiAD/Program.cs
@@ -4,28 +4,32 @@ using System.Linq;
 namespace SiAD
 {
 
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
-      ISort algorithm = null;
-      string algo="insertion";
-      switch (algo)
-      {
-      case "insertion": algorithm = new InsertionSort();
-        break;
-      case "merge": algorithm = new InsertionSort();
-        break;
-       
-      }
+        public static void Main(string[] args)
+        {
+            ISort algorithm = null;
+            string algo = "insertion";
+            switch (algo)
+            {
+                case "insertion":
+                    algorithm = new InsertSecond();
+                    break;
+                case "merge":
+                    algorithm = new InsertionSort();
+                    break;
 
-      if (algorithm != null)
-      {
-           var output = algorithm.sort(new int[]{2,5,1,76,2,9,56,124,4,5,8});
-           Console.WriteLine(String.Join(",",output));
-           
-      }
+            }
 
+            if (algorithm != null)
+            {//,76,2,9,56,124,4,5,8
+                ((InsertSecond)algorithm).SComparator = ((InsertSecond)algorithm).less;
+                var output = algorithm.sort(new int[] { 2, 5, 8, 1 });
+                Console.WriteLine(String.Join(",", output));
+
+            }
+            Console.Read();
+
+        }
     }
-  }
 }
